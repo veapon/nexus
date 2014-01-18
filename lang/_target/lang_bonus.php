44056256607690d8b4d6ffbab333146287a29dfe
hd4fans <hd4fans@gmail.com>
2014-01-19 00:00:08 +0800

    auto commit  2014-01-19	Sunday	00:00:08

---
+++ lang/_target/lang_bonus.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang_bonus_target = array
+(
+	'en' => array
+	(
+	'msg_post_bonus' => "Your forum post got bonus.",
+	'msg_post_bonus_detail' => "Your post\n\n[url=forums.php?action=viewtopic&topicid=%d&page=p%d#pid%d]Re: \"%s\"[/url]\n\nwas awarded by [b]%s[/b] [b][color=red]%d[/color][/b] bonus points.",
+	),
+	'chs' => array
+	(
+	'msg_post_bonus' => "您在帖子获赠魔力值",
+	'msg_post_bonus_detail' => "您在帖子\n\n[url=forums.php?action=viewtopic&topicid=%d&page=p%d#pid%d]回复: \"%s\"[/url]\n\n获得[b]%s[/b]赠送的[b][color=red]%d[/color][/b]点魔力值。",
+	),
+	'cht' => array
+	(
+	'msg_post_bonus' => "您在帖子獲贈魔力值",
+	'msg_post_bonus_detail' => "您在帖子\n\n[url=forums.php?action=viewtopic&topicid=%d&page=p%d#pid%d]回複: \"%s\"[/url]\n\n獲得[b]%s[/b]贈送的[b][color=red]%d[/color][/b]點魔力值。",
+	),
+);
+
+?>
